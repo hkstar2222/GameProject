1b416ab2d7d0ff7eb7d288084da8d23550d51d8d
DevilSoulcz <542724465@qq.com>
2016-09-26 03:05:16 +0800

    修改项目配置
    修改dll文件

--- GameProject/Product/DbServer/DbServer.h
+++ GameProject/Product/DbServer/DbServer.h
@@ -1,19 +1,14 @@
+#ifndef _DB_SERVER_H_
+#define _DB_SERVER_H_
 
-#include <stdio.h>
+#include "BaseDef.h"
 #include <string>
-#include <afxsock.h>
-#include "mysql.h"
-using namespace std;
 
 class DbServer
 {
 public:
-	DbServer();
-	~DbServer();
-public:
-
-   //变量
-   MYSQL mysql;
+	DLL_API DbServer();
+	DLL_API ~DbServer();
    /*
    主要的功能：
    初始化数据库
@@ -21,7 +16,7 @@ public:
    设置字符集
 
    入口参数：
-   host ：MYSQL服务器IP
+   host ：服务器IP
    port:数据库端口
    Db：数据库名称
    user：数据库用户
@@ -32,7 +27,7 @@ public:
    出口参数：
    int ：0表示成功；1表示失败
    */
-   int ConnMySQL(char *host,char * port,char * Db,char * user,char* passwd,char * charset,char * Msg);
+	DLL_API int ConnMySQL(char *host, char * port, char * Db, char * user, char* passwd, char * charset, char * Msg);
 
    /*
    主要的功能：
@@ -40,18 +35,17 @@ public:
 
    入口参数：
    SQL：查询的SQL语句
-   Cnum:查询的列数
    Msg:返回的消息，包括错误消息
 
    出口参数：
-   string 准备放置返回的数据，多条记录则用0x06隔开,多个栏位用0x05隔开
-   如果 返回的长度＝ 0，责表示舞结果
+   string 准备放置返回的数据，多条记录则用0x20隔开,多个栏位用0x20隔开
+   如果 返回的长度＝ 0，则表示无结果
    */
-   string SelectData(char * SQL,int Cnum ,char * Msg);
+	DLL_API std::string SelectData(char * SQL, char * Msg);
 
    /*
    主要功能：
-   插入数据
+   执行指定sql语句
 
    入口参数
    SQL：查询的SQL语句
@@ -60,40 +54,19 @@ public:
    出口参数：
    int ：0表示成功；1表示失败
    */
-   int InsertData(char * SQL,char * Msg);
-
-   /*
-   主要功能：
-   修改数据
-
-   入口参数
-   SQL：查询的SQL语句
-   Msg:返回的消息，包括错误消息
-
-   出口参数：
-   int ：0表示成功；1表示失败
-   */
-   int UpdateData(char * SQL,char * Msg);
-
-
-   /*
-   主要功能：
-   删除数据
-
-   入口参数
-   SQL：查询的SQL语句
-   Msg:返回的消息，包括错误消息
-
-   出口参数：
-   int ：0表示成功；1表示失败
-   */
-   int DeleteData(char * SQL,char * Msg);
+	DLL_API int MySQL_Qyery(char * SQL, char * Msg);
 
    /*
    主要功能：
    关闭数据库连接
    */
-   void CloseMySQLConn();
+	DLL_API void CloseMySQLConn();
 
+private:
+	/*
+	参数
+	*/
+	void*	m_sql;
 };
 
+#endif	//_DB_SERVER_H_
---
+++ GameProject/UnitTest/BaseTest/DbServer.h
@@ -0,0 +1,8 @@
+#pragma once
+class DbServer
+{
+public:
+	DbServer();
+	~DbServer();
+};
+
